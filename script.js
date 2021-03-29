48720dd10a395ece2ff09a7cec04a69e011b5a63
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-28 22:06:39 -0600

    finished

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const generateButton = document.querySelector('.generate-tree-button');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
-let curve;
+let curve = 10;
+let curve2 = 0;
 
 function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
   ctx.beginPath();
@@ -20,14 +21,14 @@ function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
   ctx.moveTo(0,0);
   // ctx.lineTo(0, -len);
   if(angle > 0){
-    ctx.bezierCurveTo(10, -len/2, 10, -len/2, 0, -len)
+    ctx.bezierCurveTo(curve2, -len/2, curve2, -len/2, 0, -len)
   } else {
-    ctx.bezierCurveTo(10, -len/2, -10, -len/2, 0, -len)
+    ctx.bezierCurveTo(curve2, -len/2, -curve2, -len/2, 0, -len)
 
   }
   ctx.stroke();
 
-  if (len < 15) {
+  if (len < 10) {
     //leafs
     ctx.beginPath();
     ctx.arc(0, -len, 20, 0, Math.PI/2);
@@ -36,7 +37,6 @@ function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
     ctx.restore();
     return;
   }
-  curve = (Math.random() * 10) + 10;
 
   drawTree(0, -len, len * 0.75, angle + curve, branchWidth * 0.6);
   drawTree(0, -len, len * 0.75, angle - curve, branchWidth * 0.6);
@@ -57,6 +57,8 @@ function generateRandomTree() {
   let color2 = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
 
   generateButton.style.background = color1
+  curve = (Math.random() * 20) + 2;
+  curve2 = Math.random() * 50;
 
   drawTree(centerPointX, canvas.height - 80, len, angle, branchWidth, color1, color2);
 }
