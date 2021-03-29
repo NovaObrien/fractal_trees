1907c31faae7eff8886791d5778da29c7830dd49
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-28 21:02:33 -0600

    Random generate working

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ const generateButton = document.querySelector('.generate-tree-button');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
+let curve;
 
 function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
   ctx.beginPath();
   ctx.save();
   ctx.strokeStyle = color1;
   ctx.fillStyle = color2;
+
+  ctx.shadowBlur = 15;
+  ctx.shadowColor = 'grey';
+
   ctx.lineWidth = branchWidth;
   ctx.translate(startX, startY);
   ctx.rotate(angle * Math.PI/180);
   ctx.moveTo(0,0);
   // ctx.lineTo(0, -len);
-  ctx.bezierCurveTo(10, -len/2, 10, -len/2, 0, -len)
+  if(angle > 0){
+    ctx.bezierCurveTo(10, -len/2, 10, -len/2, 0, -len)
+  } else {
+    ctx.bezierCurveTo(10, -len/2, -10, -len/2, 0, -len)
+
+  }
   ctx.stroke();
 
   if (len < 15) {
@@ -26,11 +36,29 @@ function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
     ctx.restore();
     return;
   }
+  curve = (Math.random() * 10) + 10;
 
-  drawTree(0, -len, len * 0.75, angle + 7, branchWidth * 0.6);
-  drawTree(0, -len, len * 0.75, angle - 7, branchWidth * 0.6);
+  drawTree(0, -len, len * 0.75, angle + curve, branchWidth * 0.6);
+  drawTree(0, -len, len * 0.75, angle - curve, branchWidth * 0.6);
 
   ctx.restore();
 }
 
 drawTree(canvas.width/2, canvas.height - 80, 120, 0, 20, 'brown', 'green');
+
+function generateRandomTree() {
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+
+  let centerPointX = canvas.width/2;
+  let len = Math.floor((Math.random() * 20) + 100);
+  let angle = 0;
+  let branchWidth = (Math.random() * 70) + 1;
+  let color1 = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+  let color2 = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+
+  generateButton.style.background = color1
+
+  drawTree(centerPointX, canvas.height - 80, len, angle, branchWidth, color1, color2);
+}
+
+generateButton.addEventListener('click', generateRandomTree)
